40252fecdf359e0081ec1b14c3c2999dc11bbc7a
LTNTSAD <ammar25491@gmail.com>
2026-04-07 16:08:24 +0700

    Add files via upload

---
+++ fundamental/15_getElement.js
@@ -0,0 +1,4 @@
+document.getElementById("result").innerHTML = "Hello World";
+document.getElementById("result").style.backgroundColor = "red";
+
+document.getElementsByTagName("h1")[0].style.backgroundColor = "blue"; 
