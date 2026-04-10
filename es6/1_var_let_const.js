6c578c254dd6091615fbdcc0c4176a366e58daf5
LTNTSAD <ammar25491@gmail.com>
2026-04-10 15:44:49 +0700

    Add files via upload

---
+++ es6/1_var_let_const.js
@@ -0,0 +1,57 @@
+//var name = "John"
+/*{    
+    var name = "Jany"
+    console.warn(name);
+}*/
+
+//function a() {
+//    var name = "Jany"
+//    console.warn(name);
+//}
+//
+//console.warn(name);
+
+//var message = ["Hello", 'JavaScript', `ES2015`];
+//for (var i = 0; i < message.length; i++) {
+//    console.warn(i);
+//    setTimeout(function() {
+//        console.warn(i);
+//        console.warn(message[i]);
+//    }, 1000)
+//
+//}
+
+//let fullname;
+//let fullname;
+//console.log(fullname);
+
+//var name = "John"
+//{    
+//    var name = "Jany"
+//    console.warn(name);
+//}
+
+//console.log(name);
+
+//let message = ["Hello", 'JavaScript', `ES2015`];
+//for (var i = 0; i < message.length; i++) {
+//    //console.log(i);
+//    setTimeout(function() {
+//        //console.warn(i);
+//        console.log(message[i]);
+//    }, 1000)
+//}
+
+/*let firstname
+console.log(firstname);*/
+
+/*const firstname = "samit"
+
+firstname = "somchai"
+console.log(firstname);*/
+
+const person = {}
+person.name = "Somkid"
+person.email = "somkid@example.com"
+
+console.log(person);
