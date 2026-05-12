2712bd91996413ea28b13ae6ce0b751190f6d85c
LTNTSAD <ammar25491@gmail.com>
2026-05-12 16:35:42 +0700

    Add files via upload

--- es6/4_template_string.js
+++ es6/4_template_string.js
@@ -1,23 +1,19 @@
+const content = "Hello, World!";
 const htmlContent = `<html>`+
     `<head>`+
         `<title>My App</title>`+
     `</head>`+
-    `<body>`+
-        `<h1>Welcome to My App</h1>`+
-        `<p>This is a simple template string example.</p>`+
-    `</body>`+
+    `<body>`+ content +`</body>`+
 `</html>`;
 
 const htmlContentnew = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ES6 Learning Fundamentals</title>
 </head>
 <body>
-    <h1>ES6 Learning Fundamentals</h1>
+    ${content}
 </body>
 </html>
 `;
